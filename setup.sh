fdcbbaaeb09a614bd20401be54ab043e714de903
flanker45 <samuel.chow.45@gmail.com>
2025-03-31 16:47:11 -0400

    add uv

--- setup.sh
+++
@@ -1,8 +0,0 @@
-# Run this script to setup stuff without having to reinstall everything
-
-python -m venv venv
-source ./venv/bin/activate
-pip install -r requirements.txt
-python manage.py makemigrations blog
-python manage.py migrate
-python manage.py createsuperuser
